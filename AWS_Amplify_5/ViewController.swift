e2474526093c8232cf1662f41734b0525197aa2f
keishinz <50899408+keishinz@users.noreply.github.com>
2019-07-04 20:37:07 +0900

    Initial Commit

---
+++ AWS_Amplify_5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWS_Amplify_5
+//
+//  Created by 張 渓岑 on 2019/07/04.
+//  Copyright © 2019 kschou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
