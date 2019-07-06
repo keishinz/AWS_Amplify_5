a232854c9671064905a40b068314b1c79f3f4562
keishinz <50899408+keishinz@users.noreply.github.com>
2019-07-06 23:18:26 +0900

    comfirmcode dispatch

---
+++ AWS_Amplify_5/SignUpOKVC.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpOKVC.swift
+//  AWS_Amplify_5
+//
+//  Created by 張 渓岑 on 2019/07/06.
+//  Copyright © 2019 kschou. All rights reserved.
+//
+
+import UIKit
+
+class SignUpOKVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var signUpOKLabel: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
